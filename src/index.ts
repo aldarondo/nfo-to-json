26b5af4b9e465faebf69d95c723092daa1e8a5dd
Charles Aldarondo <charles.aldarondo@gmail.com>
2026-02-28 19:28:40 -0700

    feat: export public API from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from "./parser";
+export { parseNfo } from './parser.js';
+export { crawlDir } from './crawler.js';
+export { buildMoviesJson, buildTvShowsJson } from './aggregator.js';
+export { run } from './runner.js';
+export type { ParsedNfo, TvShowSummary, MovieData, EpisodeData, ShowData, ActorData, BaseMediaData } from './types.js';
